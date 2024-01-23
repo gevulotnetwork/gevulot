6afedc6484319d8fda4a9a0ca84814195e493f2b
Tuomas Mäkinen <1947505+tuommaki@users.noreply.github.com>
2024-01-23 11:05:52 +0200

    Don't broadcast propagated transaction (#49)
    If transaction has already been propagated (tx.propagated == true),
    don't send it to P2P network anymore.

--- crates/node/src/mempool/mod.rs
+++ crates/node/src/mempool/mod.rs
@@ -80,12 +80,14 @@ impl Mempool {
         self.storage.set(&tx).await?;
 
         // Broadcast new transaction to P2P network if it's configured.
-        if let Some(ref tx_chan) = self.tx_chan {
-            if tx_chan.send_tx(&tx).await.is_ok() {
-                tx.propagated = true;
-                self.storage.set(&tx).await?;
-            } else {
-                // TODO: Implement retry?
+        if !tx.propagated {
+            if let Some(ref tx_chan) = self.tx_chan {
+                if tx_chan.send_tx(&tx).await.is_ok() {
+                    tx.propagated = true;
+                    self.storage.set(&tx).await?;
+                } else {
+                    // TODO: Implement retry?
+                }
             }
         }
 
