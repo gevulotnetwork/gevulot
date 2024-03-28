1e1d1ead0f20e0a341563f700985ec2a4728d9ff
musitdev <philippe.delrieu@free.fr>
2024-03-28 10:42:57 +0100

    add no scheduler mempool pick task detection

--- crates/node/src/watchdog/mod.rs
+++ crates/node/src/watchdog/mod.rs
@@ -6,6 +6,7 @@ use hyper_util::rt::TokioIo;
 use std::net::SocketAddr;
 use std::sync::Arc;
 use tokio::net::TcpListener;
+use tokio::select;
 use tokio::sync::mpsc;
 use tokio::sync::Mutex;
 use tokio::time::timeout;
@@ -13,6 +14,7 @@ use tokio::time::Duration;
 
 pub const GRACEFULL_SIGNAL_COUNTER_LIMIT: usize = 20;
 pub const SCHEDULER_HEATH_SIGNAL_TIMEOUT_MILLI: Duration = Duration::from_millis(1000);
+pub const NO_LOOP_DETECT_TIMEOUT_MILLI: Duration = Duration::from_millis(1000);
 
 #[derive(Clone, Debug, Copy, PartialEq, PartialOrd)]
 pub enum HealthCheckSignal {
@@ -46,6 +48,7 @@ pub async fn start_watchdog(bind_addr: SocketAddr) -> Result<mpsc::Sender<Health
             run_watchdog(
                 GRACEFULL_SIGNAL_COUNTER_LIMIT,
                 SCHEDULER_HEATH_SIGNAL_TIMEOUT_MILLI,
+                NO_LOOP_DETECT_TIMEOUT_MILLI,
                 watchdog_state,
                 scheduler_health_rx,
             )
@@ -60,40 +63,57 @@ pub async fn start_watchdog(bind_addr: SocketAddr) -> Result<mpsc::Sender<Health
 async fn run_watchdog(
     gracefull_signal_counter_limit: usize,
     scheduler_heath_signal_timeout: Duration,
+    no_loop_detect_timeout: Duration,
     watchdog_state: Arc<Mutex<WatchDogState>>,
     mut scheduler_health_rx: mpsc::Receiver<HealthCheckSignal>,
 ) {
     let mut scheduler_state = (true, true, true);
 
+    // Define a timer to detect when the mempool pick_task is never call but the loop is working.
+    let mut no_loop_detect_timer = tokio::time::interval(no_loop_detect_timeout);
+    let mut see_scheduler_loop_ok = true;
+
     let mut timout_counter = 0;
 
     loop {
         let mut new_state = scheduler_state;
-        match timeout(scheduler_heath_signal_timeout, scheduler_health_rx.recv()).await {
-            Ok(res) => {
-                //The scheduler signal is alive
-                timout_counter = 0;
-                new_state.2 = true;
-                match res {
-                    Some(msg) => match msg {
-                        HealthCheckSignal::SchedulerLoopOk => new_state.0 = true,
-                        HealthCheckSignal::SchedulerMempoolLen(len) => new_state.1 = len == 0,
-                    },
-                    None => {
-                        tracing::error!(
-                                    "Scheduler Health channel error, Health channel closed. Stop health check."
-                                );
-                        // Alone receiver won't get any message. recv always timeout.
-                        (_, scheduler_health_rx) = tokio::sync::mpsc::channel(1);
-                        new_state.2 = false;
+        select! {
+             _ = no_loop_detect_timer.tick() => {
+                // No loop ok since last call. Set scheduler loop issue
+                if !see_scheduler_loop_ok {
+                    new_state.0 = false
+                }
+                see_scheduler_loop_ok = false;
+             }
+            res = timeout(scheduler_heath_signal_timeout, scheduler_health_rx.recv()) => match res {
+                Ok(res) => {
+                    //The scheduler signal is alive
+                    timout_counter = 0;
+                    new_state.2 = true;
+                    match res {
+                        Some(msg) => match msg {
+                            HealthCheckSignal::SchedulerLoopOk => {
+                                see_scheduler_loop_ok = true;
+                                new_state.0 = true
+                            },
+                            HealthCheckSignal::SchedulerMempoolLen(len) => new_state.1 = len == 0,
+                        },
+                        None => {
+                            tracing::error!(
+                                        "Scheduler Health channel error, Health channel closed. Stop health check."
+                                    );
+                            // Alone receiver won't get any message. recv always timeout.
+                            (_, scheduler_health_rx) = tokio::sync::mpsc::channel(1);
+                            new_state.2 = false;
+                        }
                     }
                 }
-            }
-            Err(_) => {
-                tracing::error!("Scheduler Health signal Timeout, no health signal available.");
-                timout_counter += 1;
-                if timout_counter >= gracefull_signal_counter_limit {
-                    new_state.2 = false;
+                Err(_) => {
+                    tracing::error!("Scheduler Health signal Timeout, no health signal available.");
+                    timout_counter += 1;
+                    if timout_counter >= gracefull_signal_counter_limit {
+                        new_state.2 = false;
+                    }
                 }
             }
         }
@@ -195,6 +215,7 @@ mod tests {
                 run_watchdog(
                     2,
                     Duration::from_millis(100),
+                    Duration::from_millis(300),
                     watchdog_state,
                     scheduler_health_rx,
                 )
@@ -220,6 +241,12 @@ mod tests {
             .unwrap();
         sleep(Duration::from_millis(20)).await;
         assert_eq!(WatchDogState::Critical, *watchdog_state.lock().await);
+        //reset loop no detect time
+        scheduler_health_tx
+            .send(HealthCheckSignal::SchedulerLoopOk)
+            .await
+            .unwrap();
+        //set state to Alive.
         scheduler_health_tx
             .send(HealthCheckSignal::SchedulerMempoolLen(0))
             .await
@@ -230,6 +257,16 @@ mod tests {
         assert_eq!(WatchDogState::Alive, *watchdog_state.lock().await);
         sleep(Duration::from_millis(100)).await;
         assert_eq!(WatchDogState::Graceful, *watchdog_state.lock().await);
+        scheduler_health_tx
+            .send(HealthCheckSignal::SchedulerMempoolLen(0))
+            .await
+            .unwrap();
+        sleep(Duration::from_millis(20)).await;
+        assert_eq!(WatchDogState::Alive, *watchdog_state.lock().await);
+        // Detect no loop
+        sleep(Duration::from_millis(350)).await;
+        assert_eq!(WatchDogState::Critical, *watchdog_state.lock().await);
+        // Return to normal state.
         scheduler_health_tx
             .send(HealthCheckSignal::SchedulerLoopOk)
             .await
