2ad84c657b702b4fbe0266734ce4a635a903a609
musitdev <philippe.delrieu@free.fr>
2024-03-29 17:17:50 +0100

    add program id Tx dep missing detection

--- crates/node/src/txvalidation/mod.rs
+++ crates/node/src/txvalidation/mod.rs
@@ -1,10 +1,10 @@
-use crate::mempool::Storage;
 use crate::txvalidation::event::TxCache;
 use crate::txvalidation::event::{ReceivedTx, TxEvent};
 use crate::types::{
     transaction::{Created, Received, Validated},
-    Transaction,
+    Hash, Program, Transaction,
 };
+use async_trait::async_trait;
 use futures::stream::FuturesUnordered;
 use futures_util::Stream;
 use futures_util::TryFutureExt;
@@ -33,6 +33,12 @@ mod event;
 #[async_trait::async_trait]
 pub trait ValidatedTxReceiver: Send + Sync {
     async fn send_new_tx(&mut self, tx: Transaction<Validated>) -> eyre::Result<()>;
+}
+
+#[async_trait]
+pub trait ValidateStorage: Send + Sync {
+    async fn get_tx(&self, hash: &Hash) -> eyre::Result<Option<Transaction<Validated>>>;
+    async fn contains_program(&self, hash: Hash) -> eyre::Result<bool>;
 }
 
 #[allow(clippy::enum_variant_names)]
@@ -139,7 +145,7 @@ pub async fn spawn_event_loop(
     mut rcv_tx_event_rx: UnboundedReceiver<(Transaction<Received>, Option<CallbackSender>)>,
     // Endpoint where validated transactions are sent to. Usually configured with Mempool.
     new_tx_receiver: Arc<RwLock<dyn ValidatedTxReceiver + 'static>>,
-    storage: Arc<impl Storage + 'static>,
+    storage: Arc<impl ValidateStorage + 'static>,
 ) -> eyre::Result<(
     JoinHandle<()>,
     // Output stream used to propagate transactions.
@@ -155,7 +161,8 @@ pub async fn spawn_event_loop(
     let p2p_stream = UnboundedReceiverStream::new(p2p_recv);
     let jh = tokio::spawn({
         let local_directory_path = local_directory_path.clone();
-        let mut wait_tx_cache = TxCache::new();
+        let mut program_wait_tx_cache = TxCache::<Program>::new();
+        let mut parent_wait_tx_cache = TxCache::<Transaction<Validated>>::new();
         let mut validated_txs_futures = FuturesUnordered::new();
         let mut validation_okresult_futures = FuturesUnordered::new();
         let mut validation_errresult_futures = FuturesUnordered::new();
@@ -202,7 +209,7 @@ pub async fn spawn_event_loop(
                     Some(Ok((wait_tx_res, callback))) = validated_txs_futures.next() =>  {
                         match wait_tx_res {
                             Ok(wait_tx) => {
-                               match wait_tx.process_event(&mut wait_tx_cache, storage.as_ref()).await {
+                               match wait_tx.process_event(&mut program_wait_tx_cache, &mut parent_wait_tx_cache, storage.as_ref()).await {
                                     Ok(new_tx_list) => {
                                         //
                                         let jh  = tokio::spawn({
