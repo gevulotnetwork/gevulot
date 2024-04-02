ea7861659d44231f742cd6256b7f951aeabb476c
musitdev <philippe.delrieu@free.fr>
2024-04-02 16:37:42 +0200

    sync Tx dep verification and tx save to be sure parent are saved before children

--- crates/node/src/txvalidation/event.rs
+++ crates/node/src/txvalidation/event.rs
@@ -312,14 +312,6 @@ impl TxEvent<WaitTx> {
             })
             .unwrap_or(vec![]);
 
-        tracing::warn!(
-            "manage_program_dep {:?}",
-            new_txs
-                .iter()
-                .map(|tx| tx.tx.hash.to_string())
-                .collect::<Vec<_>>()
-        );
-
         Ok(new_txs)
     }
 
@@ -340,13 +332,6 @@ impl TxEvent<WaitTx> {
         }
         // Only test for the first program, because all Tx program came from the same deploy tx.
         let parent = self.tx.payload.get_parent_tx().cloned();
-        tracing::warn!(
-            "manage_parent_dep parent {:?}",
-            parent
-                .iter()
-                .map(|hash| hash.to_string())
-                .collect::<Vec<_>>()
-        );
         let new_tx = self
             .wait_if_not_cached(parent_cache, parent.as_ref(), storage, query_db_for_tx)
             .await?;
