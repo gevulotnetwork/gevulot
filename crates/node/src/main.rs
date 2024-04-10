22dccad404cf7c1a57b690dafbee14d1083eff70
Brian Hechinger <wonko@4amlunch.net>
2024-04-10 20:23:48 +0100

    Just a loop wasn't enough, it goes too fast

--- crates/node/src/main.rs
+++ crates/node/src/main.rs
@@ -397,6 +397,8 @@ async fn p2p_beacon(config: P2PBeaconConfig) -> Result<()> {
                     tracing::error!("failed to resolve {}: {}", addr, err);
                 }
             }
+
+            tokio::time::sleep(Duration::from_secs(1)).await;
         }
 
         try_count += 1;
