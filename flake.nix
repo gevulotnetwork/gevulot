b786ba17c6062a84d7288fa669f897fef2c7ae31
Brian Hechinger <wonko@4amlunch.net>
2024-04-10 16:21:03 +0100

    removed packages not needed to build the node on nixos

--- flake.nix
+++ flake.nix
@@ -19,12 +19,6 @@
             pkgs.mkShell {
               buildInputs =
               [
-                p.git
-                p.opentofu
-                p.google-cloud-sdk-gce
-                (p.google-cloud-sdk.withExtraComponents [p.google-cloud-sdk.components.gke-gcloud-auth-plugin])
-                p.k9s
-                p.kube-capacity
                 p.openssl
                 p.pkg-config
                 p.protobuf
